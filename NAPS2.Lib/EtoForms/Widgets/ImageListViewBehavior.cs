035c2100f6616b3f1d0c481b0ac46316da2f7caa
ccp-net <checongphuoc@gmail.com>
2026-08-22 17:19:40 +0700

    Remove dossier labels from scan thumbnails

--- NAPS2.Lib/EtoForms/Widgets/ImageListViewBehavior.cs
+++ NAPS2.Lib/EtoForms/Widgets/ImageListViewBehavior.cs
@@ -20,19 +20,12 @@ public class ImageListViewBehavior : ListViewBehavior<UiImage>
         _thumbnailProvider = thumbnailProvider;
         _config = config;
         MultiSelect = true;
-        ShowLabels = true;
+        ShowLabels = false;
         ScrollOnDrag = true;
         UseHandCursor = true;
     }
 
     public override bool ShowPageNumbers => _config.Get(c => c.ShowPageNumbers);
-
-    public override string GetLabel(UiImage item)
-    {
-        return item.PartyDossierDocumentTypeId.HasValue
-            ? $"HS {item.PartyDossierDocumentTypeId.Value:00}"
-            : string.Empty;
-    }
 
     public override Image GetImage(IListView<UiImage> listView, UiImage item)
     {
